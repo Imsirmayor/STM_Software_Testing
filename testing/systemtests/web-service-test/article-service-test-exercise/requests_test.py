7d731c4ea686e27402d5236bc69888d7e9895a89
Imsirmayor <m.adeyemi.e@gmail.com>
2024-04-26 16:32:24 +0200

    trying two remotes

--- testing/systemtests/web-service-test/article-service-test-exercise/requests_test.py
+++ testing/systemtests/web-service-test/article-service-test-exercise/requests_test.py
@@ -3,9 +3,27 @@ import requests
 
 
 class ArticleServiceTest(unittest.TestCase):
-# TODO: Implement etst cases
-    pass 
+    def test_find_all(self):
+        # Setup
+        url = 'http://localhost:8080/articles'
+        # Exercise
+        response = requests.get(url)
+        # Verify
+        content = response.text
+        print(f"Response content: {content}")
+        status = response.status_code
+        self.assertEqual(200, status)
 
+    def test_find_by_id(self):
+        # Setup
+        url = 'http://localhost:8080/articles/1'
+        # Exercise
+        response = requests.get(url)
+        # Verify
+        content = response.text
+        print(f"Response content: {content}")
+        status = response.status_code
+        self.assertEqual(200, status)
 
 if __name__ == '__main__':
     unittest.main()
